19d2fd0d3ca046ef5ead61b78a42905fa6e0d05d
OmerCanKAPLAN <kaplan_911@hotmail.com>
2023-01-07 17:41:02 +0300

    first commit

---
+++ giphyapi/src/components/search.vue
@@ -0,0 +1,17 @@
+<!-- model binding v-model ile  -->
+<template>
+    <div class="search">
+        <input type="text" v-model="query">
+        <button>Search</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            query: ''
+        }
+    }
+}
+</script>
